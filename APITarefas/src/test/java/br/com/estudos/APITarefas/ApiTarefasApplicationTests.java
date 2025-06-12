3077509c34152449ee6cf8025a6462176ac82117
João Ganança <93109162+Joaogananca26@users.noreply.github.com>
2025-06-11 21:14:46 -0300

    Adicionando pasta do projeto

---
+++ APITarefas/src/test/java/br/com/estudos/APITarefas/ApiTarefasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.estudos.APITarefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTarefasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
