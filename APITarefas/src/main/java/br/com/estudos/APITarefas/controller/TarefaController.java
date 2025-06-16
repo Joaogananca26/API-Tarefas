220f133fcb908d6ef1cbae04174694d9fc87d583
joaogananca26 <joaogananca26@gmail.com>
2025-06-15 22:43:34 -0300

    Fazendo o cadastro de tarefas na controller

---
+++ APITarefas/src/main/java/br/com/estudos/APITarefas/controller/TarefaController.java
@@ -0,0 +1,34 @@
+package br.com.estudos.APITarefas.controller;
+
+import br.com.estudos.APITarefas.domain.dto.request.CadastrarTarefaRequest;
+import br.com.estudos.APITarefas.domain.dto.response.CadastrarTarefaResponse;
+import br.com.estudos.APITarefas.domain.entities.Tarefa;
+import br.com.estudos.APITarefas.service.TarefaService;
+import lombok.AllArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/tarefa")
+@AllArgsConstructor
+public class TarefaController {
+
+    private TarefaService service;
+    private ModelMapper mapper;
+
+    @PostMapping("/Adicionar")
+    public ResponseEntity<CadastrarTarefaResponse> cadastrarTarefa(@RequestBody CadastrarTarefaRequest dto) {
+        Tarefa tarefa = mapper.map(dto, Tarefa.class);
+        Tarefa tarefaSalva = service.cadastrarTarefa(tarefa);
+
+        CadastrarTarefaResponse response = mapper.map(tarefaSalva, CadastrarTarefaResponse.class);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+}
