061277c17d79e3c757d6b3ab9976cdaf92fd0d7b
joaogananca26 <joaogananca26@gmail.com>
2025-06-15 22:43:14 -0300

    Fazendo a classe de cadastro de tarefa response e request

---
+++ APITarefas/src/main/java/br/com/estudos/APITarefas/domain/dto/response/CadastrarTarefaResponse.java
@@ -0,0 +1,17 @@
+package br.com.estudos.APITarefas.domain.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CadastrarTarefaResponse {
+
+    private String idTarefa;
+    private String nomeTarefa;
+    private String descricaoTarefa;
+    private String nomeUsuarioTarefa;
+    private String prioridadeTarefa;
+}
