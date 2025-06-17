eba6850ac8a2e9243aa85905864d36d328387d1e
joaogananca26 <joaogananca26@gmail.com>
2025-06-16 21:23:03 -0300

    Arrumando atributos da tabela tarefa

--- APITarefas/src/main/java/br/com/estudos/APITarefas/domain/entities/Tarefa.java
+++ APITarefas/src/main/java/br/com/estudos/APITarefas/domain/entities/Tarefa.java
@@ -13,7 +13,7 @@ import lombok.NoArgsConstructor;
 public class Tarefa {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.UUID)
     @Column(name = "id_tarefa")
     private String idTarefa;
 
